5201f2d5e1fccc7d57648c307192f0631c152302
Gustavo Silv4 <contato@gustavosilv4.com.br>
2022-08-12 21:28:39 -0300

    feat: add integration with github api users

--- src/pages/Home/components/CardAbout/index.tsx
+++ src/pages/Home/components/CardAbout/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-import { apiProfile } from '../../../../lib/axios'
-import { Profile } from '../../../../utils/types'
+import { api } from '../../../../lib/axios'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBuilding, faUserGroup, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
@@ -8,12 +7,23 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import { CardAboutContainer, CardAboutContent, CardFooter, CardHeader, CardMain, ImageGithub } from './styles'
 
+interface Profile {
+  id: number
+  avatar_url: string
+  bio: string
+  company: string
+  name: string
+  login: string
+  html_url: string
+  followers: number
+}
+
 export function CardAbout() {
   const [profile, setProfile] = useState<Profile>({} as Profile)
 
   useEffect(() => {
     async function getProfileGitHub() {
-      const response = await apiProfile.get('/gustavosilv4')
+      const response = await api.get('users/gustavosilv4')
       setProfile(response.data)
     }
 
@@ -48,7 +58,7 @@ export function CardAbout() {
             </span>
             <span>
               <FontAwesomeIcon icon={faUserGroup} />
-              {profile?.followers} {profile?.followers > 1 ? 'seguidores' : 'seguidor'}
+              {profile?.followers} {profile?.followers > 1 || profile?.followers === 0 ? 'seguidores' : 'seguidor'}
             </span>
           </CardFooter>
         </div>
