bb4bbb8f36f5db3a23bee4578f567707478fe22e
Gustavo Silv4 <contato@gustavosilv4.com.br>
2022-08-12 21:22:06 -0300

    feat: create baseURL with conection github api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,5 @@
 import axios from 'axios'
 
-export const apiProfile = axios.create({
-  baseURL: 'https://api.github.com/users',
-})
-
-export const getIssuesList = axios.create({
-  baseURL: 'https://api.github.com/search/issues',
-})
-
-export const getAnIssues = axios.create({
+export const api = axios.create({
   baseURL: 'https://api.github.com',
 })
